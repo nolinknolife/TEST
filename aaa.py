a4e4bfa4edd5764d3650815c6767f1cad15fc21a
aaa <aaa@ggg.com>
2020-05-08 20:06:51 +0900

    add plt.plot

--- aaa.py
+++ aaa.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 a = 10
 print(a)
@@ -8,5 +9,8 @@ print(b)
 
 c = np.array([1,1,1])
 print(c)
+
+plt.plot(c)
+
 
 #edit at branch pochi/ a master
